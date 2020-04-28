9522ff9eb247ec5e8f0d721600ef3ad81557b1e5
mayuresh.marathe <mayuresh.marathe@NTINLTP1401054.FPS.NIHILENT.com>
2020-04-28 20:59:16 +0530

    commit

---
+++ jenkinsPipeline/src/test/java/jenkinsPipelinePackage/OccuranceOfCharacters.java
@@ -0,0 +1,14 @@
+package jenkinsPipelinePackage;
+
+import org.testng.annotations.Test;
+
+public class OccuranceOfCharacters {
+	
+	
+	@Test
+	public void print()
+	{
+		System.out.println("HIIIIIIIIIIIIIIIII");
+	}
+
+}
